1c3c18224b4d3afb49738e1dd79a4ea049aae46a
Oscar Xu <>
2022-10-31 22:15:23 -0400

    added real functionality to Vao

--- include/Vao.h
+++ include/Vao.h
@@ -2,16 +2,27 @@
 #define VAO
 
 #include <GL/glew.h>
+#include <vector>
+#include <glm/glm.hpp>
 #include "GlResource.h"
+
+struct Vertex {
+    glm::vec3 position;
+    glm::vec2 uv;
+    glm::vec3 normal;
+};
 
 class Vao : public GlResource {
 public:
     Vao();
+    Vao(const std::vector<Vertex> &vertices);
     ~Vao();
     const GLuint vaoId;
     
     inline void bind() { glBindVertexArray(vaoId); }
     inline void unbind() { glBindVertexArray(0); }
+private:
+    std::vector<GLuint> vbos;
 };
 
 #endif /* VAO */
--- src/Vao.cpp
+++ src/Vao.cpp
@@ -1,9 +1,28 @@
 #include "Vao.h"
 
-Vao::Vao() : vaoId([](){ GLuint tmp; glGenVertexArrays(1, &tmp); return tmp; }()) {
+Vao::Vao() : vaoId([](){ GLuint tmp; glGenVertexArrays(1, &tmp); return tmp; }()) {}
+
+Vao::Vao(const std::vector<Vertex> &vertices) : vaoId([](){ GLuint tmp; glGenVertexArrays(1, &tmp); return tmp; }()) {
+    GLuint vbo;
+    glGenBuffers(1, &vbo);
+    vbos.push_back(vbo);
+
+    glNamedBufferStorage(vbo, vertices.size() * sizeof(Vertex), vertices.data(), 0);
+
+    glVertexArrayVertexBuffer(vaoId, 0, vbo, 0, sizeof(Vertex));
     
+    for (int i = 0; i < 3; i++) {
+        glEnableVertexArrayAttrib(vaoId, i);
+        glVertexArrayAttribBinding(vaoId, i, 0);
+        glVertexArrayBindingDivisor(vaoId, i, 0); // if the divisor is non-zero it becomes an instanced attribute?
+    }
+
+    glVertexArrayAttribFormat(vaoId, 0, 3, GL_FLOAT, GL_FALSE, offsetof(Vertex, position));
+    glVertexArrayAttribFormat(vaoId, 1, 2, GL_FLOAT, GL_FALSE, offsetof(Vertex, uv));
+    glVertexArrayAttribFormat(vaoId, 0, 3, GL_FLOAT, GL_FALSE, offsetof(Vertex, normal));
 }
 
 Vao::~Vao() {
     glDeleteVertexArrays(1, &vaoId);
+    glDeleteBuffers(vbos.size(), vbos.data());
 }
